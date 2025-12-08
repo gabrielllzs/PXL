b704010ac436870d50807b735f72baf401b85ef1
GabrielZs <147093667+GabrielZapataSarmiento@users.noreply.github.com>
2025-12-09 00:35:57 +0100

    Add banned_users table and update users table migration
    Removed the 'email_verified_at' column from the users table migration. Added a new migration to create the 'banned_users' table with fields for visitor ID, IP address, reason, ban duration, permanence, and timestamps.

---
+++ database/migrations/2025_12_08_215553_create_banned_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banned_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('visitor_id')->unique();
+            $table->string('ip_address')->nullable();
+            $table->string('reason')->nullable();
+            $table->integer('duration_minutes')->nullable();
+            $table->boolean('is_permanent')->default(false);
+            $table->timestamp('banned_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banned_users');
+    }
+};
