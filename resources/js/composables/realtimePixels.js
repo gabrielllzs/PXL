37eb23ce33c2ac0606020c97333e45772b5b1919
gabrielllzs <147093667+gabrielllzs@users.noreply.github.com>
2025-12-26 22:23:34 +0100

    Update realtimePixels.js

--- resources/js/composables/realtimePixels.js
+++ resources/js/composables/realtimePixels.js
@@ -10,10 +10,11 @@ export function initRealtimePixels(drawAll) {
         broadcaster: "reverb",
         key: import.meta.env.VITE_REVERB_APP_KEY,
         wsHost: import.meta.env.VITE_REVERB_HOST,
-        wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+        wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+        wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
         forceTLS: false,
-        enabledTransports: ["ws", "wss"],
+        enabledTransports: ["ws"],
+        disableStats: true,
     });
 
     window.Echo.channel("pixel").listen(".PixelPlaced", (e) => {
