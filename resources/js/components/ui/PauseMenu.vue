ab0b57a783fed3b93a6580fda51cf5e67e2f17d1
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-03 16:09:07 +0100

    better pause menu

---
+++ resources/js/components/ui/PauseMenu.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="pause-menu">
+        hello
+    </div>
+</template>
+
+<style scoped>
+.pause-menu {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+}
+</style>
