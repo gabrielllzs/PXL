4597a96a8eb31ddd6bf16600b5d2160159a8d7a2
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-03 16:09:20 +0100

    cleaner imports

---
+++ resources/js/components/ui/index.js
@@ -0,0 +1,13 @@
+import ColorPicker from './ColorPicker.vue'
+import PixelInfo from './PixelInfo.vue'
+import CooldownInfo from './CooldownInfo.vue'
+import WalletConnect from './Links.vue'
+import PauseMenu from './PauseMenu.vue'
+
+export {
+    ColorPicker,
+    PixelInfo,
+    CooldownInfo,
+    PauseMenu,
+    WalletConnect,
+}
