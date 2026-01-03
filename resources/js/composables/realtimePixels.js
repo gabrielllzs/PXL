a62c934f8894555a6cb76a52472b6c25efbc1020
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-03 16:08:42 +0100

    Update realtimePixels.js
    try incase of errors

--- resources/js/composables/realtimePixels.js
+++ resources/js/composables/realtimePixels.js
@@ -5,33 +5,30 @@ import {stored} from "./usePixels.js";
 window.Pusher = Pusher
 
 export function initRealtimePixels(drawAll) {
-    // Completely disable wss transport
-    window.Echo = new Echo({
-        broadcaster: "reverb",
-        key: import.meta.env.VITE_REVERB_APP_KEY,
-        wsHost: import.meta.env.VITE_REVERB_HOST,
-        wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-        wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-        forceTLS: false,
-        enabledTransports: ["ws"],
-        disableStats: true,
-    });
+    try {
+        window.Echo = new Echo({
+            broadcaster: "reverb",
+            key: import.meta.env.VITE_REVERB_APP_KEY,
+            wsHost: import.meta.env.VITE_REVERB_HOST,
+            wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+            wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+            forceTLS: false,
+            enabledTransports: ["ws"],
+            disableStats: true,
+        });
 
-    window.Echo.channel("pixel").listen(".PixelPlaced", (e) => {
-        console.log("✅ Pixel received:", e);
+        window.Echo.channel("pixel").listen(".PixelPlaced", (e) => {
+            const index = stored.findIndex(p => p.x === e.x && p.y === e.y);
+            if (index >= 0) {
+                stored[index].color = e.color;
+            } else {
+                stored.push({ x: e.x, y: e.y, color: e.color });
+            }
 
-        const index = stored.findIndex(p => p.x === e.x && p.y === e.y);
-        if (index >= 0) {
-            stored[index].color = e.color;
-        } else {
-            stored.push({ x: e.x, y: e.y, color: e.color });
-        }
+            drawAll();
+        });
 
-        drawAll();
-    });
-
-    window.Echo.connector.pusher.connection.bind('connected', () => {
-        console.log('✅ Connected to Reverb!');
-    });
-
+    }catch (err){
+        console.log("Failed to init realtime pixels:", err);
+    }
 }
