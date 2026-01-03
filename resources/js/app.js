f3aebfd752b35724045366219ecbe16dbbf65701
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-03 16:24:35 +0100

    Add SupportForm component to app.js
    Registered the SupportForm component for use in the application by importing and defining it as an async component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,13 @@ import './bootstrap'; // Ensure axios is loaded
 const Canvas = defineAsyncComponent(() => import('./views/PixelMapView.vue'));
 const Login = defineAsyncComponent(() => import('./components/auth/login.vue'));
 const AdminPanel = defineAsyncComponent(() => import('./components/admin/AdminPanel.vue'));
+const Support = defineAsyncComponent(() => import('./components/Support/SupportForm.vue'));
 
 const app = createApp({})
 
 app.component('canvas-view', Canvas)
 app.component('login-view', Login)
 app.component('admin-panel', AdminPanel)
+app.component('support-form', Support)
 
 app.mount('#app')
