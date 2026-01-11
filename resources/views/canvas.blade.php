77ea69153484b5ad453861b17dc52c3003791953
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-11 19:18:08 +0100

    removing redundent code

--- resources/views/canvas.blade.php
+++ resources/views/canvas.blade.php
@@ -48,12 +48,15 @@
     </script>
     <meta name="description" content="Place pixels on a shared online canvas. Join the community art experiment.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="preload" href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css" as="style" crossorigin>
-    <link rel="stylesheet" href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css">
+    <link rel="stylesheet" href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css" crossorigin>
+
     <link rel="dns-prefetch" href="https://js.hcaptcha.com">
-    <link rel="dns-prefetch" href="https://unpkg.com">
-    <link rel="preload" href="/fonts/pixel-art.ttf" as="font" type="truetype" crossorigin>
+
+    <link rel="preload" href="/fonts/pixel-art.ttf" as="font" type="font/ttf" crossorigin>
 
     <style> body { margin: 0; padding: 0; background: #fff; } </style>
     <title>Pixel Place</title>
