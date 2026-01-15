0ab42f60c6d91742671d1a43fd1c08653be975e3
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 23:06:09 +0100

    Update 0001_01_01_000000_create_users_table.php
    Added the table for the email verification

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,10 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('country', 2)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email_verification_code', 6)->nullable();
+            $table->timestamp('email_verification_code_expires_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
