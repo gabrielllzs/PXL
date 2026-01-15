b3d6ea0ede11ba1656dec5c9b3d7ebde5ee1301d
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 22:11:37 +0100

    Replaced Support with feedback

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,13 +5,13 @@ import './bootstrap'; // Ensure axios is loaded
 const Canvas = defineAsyncComponent(() => import('./views/PixelMapView.vue'));
 const Login = defineAsyncComponent(() => import('./components/auth/login.vue'));
 const AdminPanel = defineAsyncComponent(() => import('./components/admin/AdminPanel.vue'));
-const Support = defineAsyncComponent(() => import('./components/support/SupportForm.vue'));
+const Feedback = defineAsyncComponent(() => import('./components/Feedback/FeedbackView.vue'));
 
 const app = createApp({})
 
 app.component('canvas-view', Canvas)
 app.component('login-view', Login)
 app.component('admin-panel', AdminPanel)
-app.component('support-form', Support)
+app.component('feedback-view', Feedback)
 
 app.mount('#app')
