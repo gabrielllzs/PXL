22ebd9d625851c9e13d5d70d92dd3fc44547c8ac
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 23:21:51 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,12 @@ import './bootstrap'; // Ensure axios is loaded
 
 
 const Canvas = defineAsyncComponent(() => import('./views/PixelMapView.vue'));
-const Login = defineAsyncComponent(() => import('./components/auth/login.vue'));
 const AdminPanel = defineAsyncComponent(() => import('./components/admin/AdminPanel.vue'));
 const Feedback = defineAsyncComponent(() => import('./components/Feedback/FeedbackView.vue'));
 
 const app = createApp({})
 
 app.component('canvas-view', Canvas)
-app.component('login-view', Login)
 app.component('admin-panel', AdminPanel)
 app.component('feedback-view', Feedback)
 
