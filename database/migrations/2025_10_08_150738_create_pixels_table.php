a90712bf732e273164b5a02b6864c9c7b0bf32d5
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 23:05:48 +0100

    Add user_id to pixels table
    user_i to the pixels table with onDelete set to 'set null'

--- database/migrations/2025_10_08_150738_create_pixels_table.php
+++ database/migrations/2025_10_08_150738_create_pixels_table.php
@@ -13,14 +13,15 @@ return new class extends Migration {
             $table->integer('x');
             $table->integer('y');
             $table->string('visitor_id');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
             $table->unsignedTinyInteger('risk_score')->default(0);
             $table->string('ip_address');
             $table->string('color')->default('black');
             $table->boolean('hidden')->default(false);
             $table->timestamps();
 
-
             $table->index(['visitor_id', 'id']);
+            $table->index('user_id');
             $table->index('created_at');
             $table->unique(['x', 'y']);
         });
