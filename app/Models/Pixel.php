dc49ad7ef669ca1fa49d77322f071e0038edf553
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 18:49:44 +0100

    Update Pixel.php
    Pixels can now belong to real users easier to identify

--- app/Models/Pixel.php
+++ app/Models/Pixel.php
@@ -8,9 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pixel extends Model
 {
-    protected $fillable = ['x', 'y', 'color', 'visitor_id', 'risk_score', 'ip_address'];
+    protected $fillable = ['x', 'y', 'color', 'visitor_id', 'user_id', 'risk_score', 'ip_address'];
 
     protected $casts = [
         'risk_score' => 'integer',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
