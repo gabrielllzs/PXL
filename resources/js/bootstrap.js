42c950dba256dce842b8bb2b5134327831761db2
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 23:11:19 +0100

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL || 'pixel-art.design'
 axios.defaults.withCredentials = true
 axios.defaults.headers.common.Accept = 'application/json'
 
