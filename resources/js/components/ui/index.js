94699145b3e1fd319df32b91c40f9d629130c49f
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-15 20:54:23 +0100

    Update index.js

--- resources/js/components/ui/index.js
+++ resources/js/components/ui/index.js
@@ -3,13 +3,13 @@ import PixelInfo from './PixelInfo.vue'
 import CooldownInfo from './CooldownInfo.vue'
 import Auth from './Auth.vue'
 import Links from './Links.vue'
-import PauseMenu from './PauseMenu.vue'
+import SideMenu from './SideMenu.vue'
 
 export {
     ColorPicker,
     PixelInfo,
     CooldownInfo,
-    PauseMenu,
     Auth,
     Links,
+    SideMenu,
 }
