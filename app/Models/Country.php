d2984bf166462f9abcaad36394a65769d7ec3697
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-19 01:45:27 +0100

    Create Country.php

---
+++ app/Models/Country.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $table = 'country';
+
+    protected $fillable = [
+        'country_code',
+        'pixels_placed',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'country_code', 'country_code');
+    }
+}
