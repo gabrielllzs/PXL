b9962109b9084638e7eded23ac4c256d287f630a
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-20 16:06:12 +0100

    Add GroupCursorMoved event and group channel auth
    Introduces the GroupCursorMoved event for broadcasting cursor movements within a group over a private channel. Also adds authorization logic for the 'group.{groupId}' broadcast channel to ensure only group members can listen.

---
+++ app/Events/GroupCursorMoved.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class GroupCursorMoved implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $x;
+    public $y;
+    public $username;
+    public $groupId;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($x, $y, $username, $groupId)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->username = $username;
+        $this->groupId = $groupId;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [new PrivateChannel('group.' . $this->groupId)];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'GroupCursorMoved';
+    }
+}
