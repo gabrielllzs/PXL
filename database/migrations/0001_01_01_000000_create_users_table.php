9891216535264022137461a652278fae15687a6b
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-24 22:33:34 +0100

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('new_email')->nullable();
             $table->string('password');
             $table->string('country', 2)->nullable();
             $table->integer('pixels_placed')->default(0)->index();
