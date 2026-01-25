4b45b8f7a438c28ed114d3611113fd33c77f6ce8
Gabriel <147093667+gabrielllzs@users.noreply.github.com>
2026-01-25 02:12:25 +0100

    Create 2026_01_25_010418_create_pixel_history_table.php
    store x,y and color data

---
+++ database/migrations/2026_01_25_010418_create_pixel_history_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pixel_history', function (Blueprint $table) {
+            $table->id();
+            $table->integer('x');
+            $table->integer('y');
+            $table->string('color');
+            $table->timestamps();
+            
+            $table->index(['x', 'y', 'created_at']); // Index for faster queries on pixel coordinates and time
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pixel_history');
+    }
+};
